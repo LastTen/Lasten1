c6e4762bfbe31e6620ca796721f12105c5aed8f0
LastTen <romastas1996@gmail.com>
2020-05-30 22:41:14 +0300

    .

--- Формула Пром_ 2_1.py
+++ Формула Пром_ 2_1.py
@@ -1,7 +1,9 @@
+
+import sys
 import math
 
-a = float(input("a = "))
-b = float(input("b = "))
+a = float(sys.argv[1])
+b = float(sys.argv[2])
 
-x1 = math.sqrt(a * b) / (math.exp(a) * b) + a * math.exp(2 * a / b)
-print(x1)
+x = math.sqrt(a * b) / (math.exp(a) * b) + a * math.exp(2*a/b)
+print(x)
